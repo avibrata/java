a1144c45e5f1c1aa0ee367eabd1967803f3eac74
Avibrata Saha Ratul <124505554+avibrata@users.noreply.github.com>
2023-06-14 21:48:42 +0600

    Add files via upload

---
+++ TryCatch/exhaneling.java
@@ -0,0 +1,29 @@
+package exeption;
+
+
+
+public class exhaneling {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int ar[] = new int[5];
+        try {
+
+
+           ar[10]=32;
+
+        }catch (Exception e){
+
+            System.out.println(e);
+        }
+
+
+
+
+        System.out.println("Welcome" );
+    }
+
+
+
+
+}
